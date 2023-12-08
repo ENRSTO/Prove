6d2e4daed26337d4f8d22bd4da5239bbc5e6c510
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2023-12-08 12:22:45 +0100

    Funciotn

--- Prove/src/MainP.java
+++ Prove/src/MainP.java
@@ -1,18 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MainP {
 	
-	public static void printCities() {
-	    
-	    List<String> cities = new ArrayList<>();
-	    cities.add("Delhi");
-	    cities.add("Mumbai");
-	    cities.add("Goa");
-	    cities.add("Pune");
-	    
+	static final List<String> cities = Arrays.asList("Delhi", "Mumbai", "Goa", "Pune");
+	
+	public static void printCities(List<String> cities) {
 
 	    Predicate<String> filterCity = city -> city.equals("Mumbai") || city.equals("Pune");
 	    // restituisce un boolena
@@ -23,19 +20,19 @@ public class MainP {
 	  
 		cities.stream().filter(filterCity).forEach(printConsumer);
 	    
-	    
-//	    
-//	    Consumer<String> printConsumer = city-> {
-//	    	String cityf = "->"+city+"<-";
-//	    	System.out.println(cityf);
-//	    };
-	    
-	    // qui il corpo della lambda defisce l'implementazione del metodo accept del consumer 
-	    
-	    //cities.forEach(printConsumer);
-	    
-	    // 
 	}	
+	
+	public static void printFirstCharOfCities(List<String> cities) {
+		
+
+	    Function<String, Character> getFirstCharFunction = city -> {
+	        return city.charAt(0);
+	    };
+	    cities.stream().map(getFirstCharFunction)
+	                   .forEach(System.out::println);
+		
+	}
+	
 
 	public static void main(String[] args) {
 		int i = 0;
@@ -46,7 +43,8 @@ public class MainP {
 		
 		c1.accept(i);
 //
-		printCities();
+		printCities(cities);
+		printFirstCharOfCities(cities);
 		
 		
 		
