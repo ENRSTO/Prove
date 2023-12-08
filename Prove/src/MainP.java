e78ccdb12817061bde4ff94f9d666543845cf8a3
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2023-12-08 11:19:29 +0100

    prove consumer

---
+++ Prove/src/MainP.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MainP {
+	
+	public static void printCities() {
+	    
+	    List<String> cities = new ArrayList<>();
+	    cities.add("Delhi");
+	    cities.add("Mumbai");
+	    cities.add("Goa");
+	    cities.add("Pune");
+	    
+	    Consumer<String> printConsumer = city-> {
+	    	String cityf = "->"+city+"<-";
+	    	System.out.println(cityf);
+	    };
+	    
+	    // qui il corpo della lambda defisce l'implementazione del metodo accept del consumer 
+	    
+	    cities.forEach(printConsumer);
+	    
+	    // 
+	}	
+
+	public static void main(String[] args) {
+		int i = 0;
+
+		Consumer<Integer> c1 = (a) -> {
+		    System.out.println(i);
+		};
+		
+		c1.accept(i);
+//
+		printCities();
+		
+		
+		
+	}
+		
+		
+
+}
