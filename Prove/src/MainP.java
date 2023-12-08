22819fd2e63c7ce886f8a9eda73f646f293e6d9b
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2023-12-08 12:05:29 +0100

    Predicate

--- Prove/src/MainP.java
+++ Prove/src/MainP.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class MainP {
 	
@@ -12,14 +13,26 @@ public class MainP {
 	    cities.add("Goa");
 	    cities.add("Pune");
 	    
+
+	    Predicate<String> filterCity = city -> city.equals("Mumbai") || city.equals("Pune");
+	    // restituisce un boolena
 	    Consumer<String> printConsumer = city-> {
 	    	String cityf = "->"+city+"<-";
 	    	System.out.println(cityf);
 	    };
+	  
+		cities.stream().filter(filterCity).forEach(printConsumer);
+	    
+	    
+//	    
+//	    Consumer<String> printConsumer = city-> {
+//	    	String cityf = "->"+city+"<-";
+//	    	System.out.println(cityf);
+//	    };
 	    
 	    // qui il corpo della lambda defisce l'implementazione del metodo accept del consumer 
 	    
-	    cities.forEach(printConsumer);
+	    //cities.forEach(printConsumer);
 	    
 	    // 
 	}	
